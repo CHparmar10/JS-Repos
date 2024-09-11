43b277c0635c308a5720f3b15244b4c4b77dd843
CHIRAG <103520519+CHparmar10@users.noreply.github.com>
2024-09-11 12:03:51 +0000

    datatypes check in

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("chirag")
+
+let name = "CHIRAG"
+let Age = 30
+let isLoggedIn = false
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => (it is a datatype) standalone value (empty value)
+// undefined => not assigned any value
+// symbol => unique
+
+//object
+console.log(typeof age);
+
+console.table([typeof null,undefined])
+
