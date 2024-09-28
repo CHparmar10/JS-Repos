fe66bde4c5a96bb19d475f2a03fe7acf6606f57e
CHIRAG <103520519+CHparmar10@users.noreply.github.com>
2024-09-28 09:50:11 +0000

    object check in

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,4 +24,34 @@ const regularUser = {
 
 //console.log(regularUser.fullname.userfullname);
 
-const obj1 = {}
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {1:"a", 2:"b"}
+
+//Notes - Object.assign() static method copies enumerable from one or more source objects to a target object it return modify target object.
+
+//const obj3 = {obj1,obj2}
+//const obj3 = Object.assign(obj1,obj2)
+const obj3 = Object.assign({},obj1,obj2)
+//console.log(obj3);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
+//console.log(Object.entries(tinderUser));
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+
+// Notes - Object destructures.
+
+const Course  = {
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+//const {courseInstructor} = Course;
+const {courseInstructor:Instructor} = Course;
+console.log(Instructor)
+
